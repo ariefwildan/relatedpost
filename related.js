86a62b4f5d4b75afef2d2d88dce3353474713d9e
ariefwildan <ariefwildanh@gmail.com>
2017-01-09 15:52:16 +0700

    Update related.js

--- related.js
+++ related.js
@@ -81,7 +81,7 @@ while (i < relatedTitles.length && i < 30 && i<maxresults) {
 document.write('<div class="more"><div class="image" id="related-img">');
 if(i!=0) document.write(' ');
 else document.write(' ');
-document.write('<a href="' + relatedUrls[r] + '"><img src="'+thumburl[r]+'"/></a></div><div class="titel"><h3><a href="' + relatedUrls[r] + '">'+relatedTitles[r]+'</a></h3>View Detail Product</div></div>');
+document.write('<a href="' + relatedUrls[r] + '"><img src="'+thumburl[r]+'"/></a></div><div class="titel"><h3><a href="' + relatedUrls[r] + '">'+relatedTitles[r]+'</a></h3></div></div>');
 
 
 if (r < relatedTitles.length - 1) {
